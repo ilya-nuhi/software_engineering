8abea6fc7d018b2578d5150cee6a3df7425e5c64
itu-itis21-nuhi18 <nuhi18@itu.edu.tr>
2021-11-12 23:47:05 +0300

    Added testing

--- se2021/server.py
+++ se2021/server.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+def encode(phrase):
+    return "...---..."
+
 @app.route("/")
 def hello():
     payload = {"message":"Hello, world"}
+    return jsonify(payload)
+@app.route("/morse/<phrase>")
+def morse(phrase):
+    payload = {"result": encode(phrase)}
     return jsonify(payload)
 
 if __name__ == "__main__":
